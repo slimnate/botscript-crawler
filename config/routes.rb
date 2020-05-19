c304dac232224d93f6b74a43cbf10b6d62ffb82f
Slim Nate <slimnatekc@gmail.com>
2020-05-19 18:46:39 -0500

    add bootstrap via webpacker, add controllers and views for clients, skills, and categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'clients#index'
+
+  resources :clients
+  resources :skills
+  resources :categories
 end
